f561a5ef2310009cf0dfba91713ddc7882e77cf6
bluefirewolf534 <bluefirewolf534@gmail.com>
2024-04-01 21:59:26 +0900

    :tada: project started

---
+++ README.md
@@ -0,0 +1,10 @@
+# NewbieWarp
+> 첫 접속한 플레이어를 특정한 워프를 이용시킴
+
+# Notice
+해당 플러그인은 `WarpCore`를 사용합니다.
+
+# How to use
+1. 서버에 NewbieWarp를 적용한 후 서버를 끈다.
+2. 플러그인 데이터 폴더에서 warpName.txt 파일에 워프 이름만 입력한다.
+3. 서버를 킨다.
---
+++ src/RoMo/NewbieWarp/NewbieWarp.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace RoMo\NewbieWarp;
+
+use pocketmine\event\EventPriority;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\plugin\PluginBase;
+use RoMo\WarpCore\warp\Warp;
+use RoMo\WarpCore\warp\WarpFactory;
+use Symfony\Component\Filesystem\Path;
+
+class NewbieWarp extends PluginBase{
+
+    /** @var Warp|null */
+    private ?Warp $warp = null;
+
+    protected function onEnable() : void{
+        $this->saveResource("warpName.txt");
+        $path = Path::join($this->getDataFolder(), "warpName.txt");
+
+        if(is_file($path)){
+            $warpName = trim(file_get_contents($path));
+            $this->warp = WarpFactory::getInstance()->getWarp($warpName);
+        }
+
+        $this->getServer()->getPluginManager()->registerEvent(PlayerJoinEvent::class, function(PlayerJoinEvent $event) : void{
+            if($this->warp === null){
+                return;
+            }
+            $player = $event->getPlayer();
+            if(!$player->hasPlayedBefore()){
+                $this->warp->teleport($player);
+            }
+        }, EventPriority::LOWEST, $this);
+        // 입장 환영 메세지와 겹치지 않게 하기 위해 LOWEST를 사용
+    }
+}
